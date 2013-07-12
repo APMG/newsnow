dce63ee84a4eb87f2358c056c19d109935f25400
Justin Heideman <jheideman@mpr.org>
2013-07-12 15:45:49 -0500

    security on login

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -3,3 +3,17 @@ Meteor.Router.add({
   '/login': 'login',
   '/submit': 'postSubmit'
 });
+
+
+
+
+//only allow access to submit page if user is logged in
+Meteor.Router.filters({
+  'requireLogin': function(page) {
+    if (Meteor.user())
+      return page;
+    else
+      return 'accessDenied';
+  }
+});
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,14 @@
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId;
+  }
+});
+
+
+
 // Posts.allow({
 //   update: ownsDocument,
 //   remove: ownsDocument
